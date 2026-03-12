b75fee8518222857158c850e7b1b45f9250de557
Simon Baev <simon@ccprotocol.xyz>
2026-03-12 17:00:38 -0600

    debug: take 5b

--- src/event_watcher.ts
+++ src/event_watcher.ts
@@ -22,33 +22,63 @@ const gpiod = bindings({ bindings: 'gpiod-wrap', module_root: `${dn}/../..` });
 //     }
 // }
 
+// function watchForEvent() {
+//     const line = workerData;
+//     if (DEBUG) {
+//         console.warn('[onoff:worker] started', { line });
+//     }
+//     while (true) {
+//         setTimeout(() => { }, 0);
+//         const status = gpiod.waitForEvent(line);
+//         if (status < 0) {
+//             throw new Error('Interrupt watcher failed');
+//         }
+//         if (status === 1) {
+//             if (DEBUG) {
+//                 try {
+//                     const mem = process.memoryUsage();
+//                     const log = `[onoff:worker] events=${eventCount}, heapMB=${Math.round(mem.heapUsed / 1024 / 1024)}, rssMB=${Math.round(mem.rss / 1024 / 1024)}, externalMB=${Math.round((mem.external || 0) / 1024 / 1024)}`;
+//                     appendFileSync(
+//                         '/tmp/onoff-worker-events.log',
+//                         `${Date.now()}: ${log}\n`
+//                     );
+//                 } catch { }
+//             }
+//             eventCount += 1;
+//             const value = gpiod.getLineValue(line);
+//             parentPort?.postMessage(value);
+//         }
+//     }
+// }
+
 function watchForEvent() {
     const line = workerData;
     if (DEBUG) {
         console.warn('[onoff:worker] started', { line });
     }
-    while (true) {
-        setTimeout(() => { }, 0);
-        const status = gpiod.waitForEvent(line);
-        if (status < 0) {
-            throw new Error('Interrupt watcher failed');
-        }
-        if (status === 1) {
-            if (DEBUG) {
-                try {
-                    const mem = process.memoryUsage();
-                    const log = `[onoff:worker] events=${eventCount}, heapMB=${Math.round(mem.heapUsed / 1024 / 1024)}, rssMB=${Math.round(mem.rss / 1024 / 1024)}, externalMB=${Math.round((mem.external || 0) / 1024 / 1024)}`;
-                    appendFileSync(
-                        '/tmp/onoff-worker-events.log',
-                        `${Date.now()}: ${log}\n`
-                    );
-                } catch { }
+    function loop() {
+        setTimeout(() => {
+            const status = gpiod.waitForEvent(line);
+            if (status < 0) {
+                throw new Error('Interrupt watcher failed');
             }
-            eventCount += 1;
-            const value = gpiod.getLineValue(line);
-            parentPort?.postMessage(value);
-        }
+            if (status === 1) {
+                if (DEBUG) {
+                    const mem = process.memoryUsage();
+                    try {
+                        const log = `[onoff:worker] events=${eventCount}, heapMB=${Math.round(mem.heapUsed / 1024 / 1024)}, rssMB=${Math.round(mem.rss / 1024 / 1024)}, externalMB=${Math.round((mem.external || 0) / 1024 /
+                            1024)}`;
+                        appendFileSync('/tmp/onoff-worker-events.log', `${Date.now()}: ${log}\n`);
+                    } catch { }
+                }
+                eventCount += 1;
+                const value = gpiod.getLineValue(line);
+                parentPort?.postMessage(value);
+            }
+            loop();
+        }, 0);
     }
+    loop();
 }
 
 watchForEvent();
